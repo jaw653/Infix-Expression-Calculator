6753ca50f45187db69e6168d857b753390e8d01f
jaw653 <jawax10@gmail.com>
2017-10-13 12:21:30 -0500

    got rid of unnecessary files

--- mytest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Runs test.c without having to compile all that bs every time
-
-make
-
-gcc -Wall -Wextra -std=c99 test.c bst.o string.o integer.o
-
-./a.out
--- testbst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Test the bst utility
-
-make
-
-gcc -Wall -Wextra -std=c99 testBST.c bst.o string.o scanner.o -o runBSTtest
-
-./runBSTtest
